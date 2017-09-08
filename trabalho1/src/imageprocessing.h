e5dfc3e92a925870ceb20e9701e582fa1ae86a0a
stephanoumenos <s_dechecchi@hotmail.com>
2017-09-08 18:09:39 -0300

    Conserta umas coisas do flex

--- trabalho1/src/imageprocessing.h
+++ trabalho1/src/imageprocessing.h
@@ -12,4 +12,6 @@ imagem abrir_imagem(char *nome_do_arquivo);
 void salvar_imagem(char *nome_do_arquivo, imagem *I);
 void liberar_imagem(imagem *i);
 
+void aplicar_brilho(imagem *I, float intensidade);
+
 #endif
